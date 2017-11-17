232b1586ee75762d90e0bab54320ba8cb8e824f9
achitchcock <achitchcock@gmail.com>
2017-11-16 22:39:26 -0800

    GameBlock creation redesign
    -Game size now scalable
    -Bounding box now generated to scale
    -GameBlocks given static colors

--- Assets/Scripts/createWorld.cs
+++ Assets/Scripts/createWorld.cs
@@ -4,28 +4,20 @@ using UnityEngine;
 
 public class createWorld : MonoBehaviour {
 
-    public List<GameObject> blocks;
-    public GameObject block;
+    public List<GameObject> blocks; 
+    public GameObject block;  // prefab
+    public GameObject boundingBox;
+    public int gameSize;
+    private int blockScale;
 
 
 	// Use this for initialization
 	void Start () {
-        Random rand = new Random();
-        for (int i = -9; i < 10; i+= 2)
-        {
-            for (int j = 1; j < 20; j+=2)
-            {
-                //back and front
-                createBlock(i,j,-11);
-                createBlock(i, j, 11);
-                //left and right
-                createBlock(-11, j, i);
-                createBlock(11, j, i);
-                //top and bottom
-                createBlock(j-10, 21, i);
-                createBlock(j-10, -1, i);
-            }
-        }
+        blockScale = 2;
+        if (gameSize < 10) { gameSize = 10; }     // ensure minimum game size
+        if (gameSize % 2 == 1) { gameSize -= 1; } //ensure game is evnly balanced
+        generateLevel();
+        placeBoundingBox();
     }
 	
 	// Update is called once per frame
@@ -33,19 +25,66 @@ public class createWorld : MonoBehaviour {
 		
 	}
 
+    void generateLevel()
+    {
+        for (int i = -gameSize; i < gameSize; i += blockScale)
+        {
+            for (int j = -gameSize; j < gameSize; j += blockScale)
+            {
+                //back and front
+                createBlock(i, j, -gameSize-blockScale, Color.red);
+                createBlock(i, j, gameSize, Color.green);
+                //left and right
+                createBlock(-gameSize-blockScale, j, i, Color.blue);
+                createBlock(gameSize, j, i, Color.yellow);
+                //top and bottom
+                createBlock(j , gameSize, i, Color.magenta);
+                createBlock(j , -gameSize-blockScale, i, Color.cyan);
+            }
+        }
+    }
 
-    void createBlock(int x, int y, int z)
+
+    void createBlock(int x, int y, int z, Color color)
     {
         // GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
         GameObject temp = (GameObject)Instantiate(block);
         temp.transform.localPosition = new Vector3(x,y,z);
         temp.transform.parent = transform;
-        temp.transform.localScale = new Vector3(2, 2, 2);
+        temp.transform.localScale = new Vector3(blockScale, blockScale, blockScale);
         temp.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
-        temp.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+        temp.GetComponent<Renderer>().material.color = color;
+        //temp.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
         blocks.Add(temp);
     }
 
+    void placeBoundingBox()
+    {
+        boundingBox.transform.localPosition = new Vector3(-blockScale, -blockScale/2, -blockScale);
+        float boundSize = (gameSize + blockScale) * 2 + blockScale;
+        List<Vector3> rotation = new List<Vector3> { new Vector3(0,0,0), new Vector3(0,90,0), new Vector3(90,0,0)  };
+        List<Vector3> translation = new List<Vector3> { new Vector3(0,0,-gameSize - 2*blockScale),
+                                                        new Vector3(-gameSize-2*blockScale,0,0),
+                                                        new Vector3(0,-gameSize - 2*blockScale, 0),
+                                                        new Vector3(0,0,gameSize+2*blockScale),
+                                                        new Vector3(gameSize+2*blockScale,0,0),
+                                                        new Vector3(0,gameSize+2*blockScale,0)};
+
+
+        List<GameObject> sides = new List<GameObject>();
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject side = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            side.transform.parent = boundingBox.transform;
+            side.transform.localScale = new Vector3(boundSize, boundSize, blockScale);
+            //side.tag = "Bound: "+ i;
+            Quaternion rot = new Quaternion();
+            rot.eulerAngles = rotation[i % 3];
+            side.transform.localRotation = rot;
+            side.transform.localPosition = translation[i];
+            sides.Add(side);
+        }
+    }
 }
 
     
